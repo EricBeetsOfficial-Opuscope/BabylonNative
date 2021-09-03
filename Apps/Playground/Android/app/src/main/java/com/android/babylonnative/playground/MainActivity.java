bcb3976c9df53bfe6fdaad39405c62bcd6a099f5
EricOnMacBookPro <eric@opuscope.com>
2021-09-03 16:49:24 +0200

    Insert a MainActivity to launch PlaygroundActivity

---
+++ Apps/Playground/Android/app/src/main/java/com/android/babylonnative/playground/MainActivity.java
@@ -0,0 +1,24 @@
+package com.android.babylonnative.playground;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlaygroundActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
